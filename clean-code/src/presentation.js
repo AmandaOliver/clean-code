fa66c5d9d822d1e69b7aa8a60484065705720267
Amanda <amanda.oliver@williamhill.com>
2019-01-20 10:37:37 +0100

    initial commit

---
+++ clean-code/src/presentation.js
@@ -0,0 +1,77 @@
+// Import React
+import React from 'react';
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text,
+} from 'spectacle';
+
+// Import theme
+import createTheme from 'spectacle/lib/themes/default';
+
+// Require CSS
+require('normalize.css');
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quaternary: '#CECECE',
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  }
+);
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Clean-Code
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            let's make it easier
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite>Author</Cite>
+          </BlockQuote>
+        </Slide>
+      </Deck>
+    );
+  }
+}
